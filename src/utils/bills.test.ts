b5dcd33849aead8bba1de361f1ae97e7904042b2
jnakaso <jnakaso@yahoo.com>
2026-03-24 12:50:21 -0700

    lint

--- src/utils/bills.test.ts
+++ src/utils/bills.test.ts
@@ -10,34 +10,34 @@ import { deriveDocumentLink, extractBillNumber, inferChamber, summarizeSponsors 
 describe("bills", () => {
 
     it("deriveDocumentLink", () => {
-        expect(deriveDocumentLink({ Id: 'test' }))
+        expect(deriveDocumentLink({ Id: 'test', BillId: 'test' }))
             .toStrictEqual({ "label": "", "url": undefined });
-        expect(deriveDocumentLink({ Id: 'test', Description: 'BILL', Url: 'EARL' }))
+        expect(deriveDocumentLink({ Id: 'test', BillId: 'test', Description: 'BILL', Url: 'EARL' }))
             .toStrictEqual({ "label": "BILL", "url": "EARL" });
     });
 
     it("inferChamber", () => {
-        expect(inferChamber({ Id: 'test' })).toBe("Unknown");
-        expect(inferChamber({ Id: 'test', Chamber: 'HOUSE' })).toBe("House");
-        expect(inferChamber({ Id: 'test', Chamber: 'Joint' })).toBe("Joint");
+        expect(inferChamber({ Id: 'test', BillId: 'test' })).toBe("Unknown");
+        expect(inferChamber({ Id: 'test', BillId: 'test', Chamber: 'HOUSE' })).toBe("House");
+        expect(inferChamber({ Id: 'test', BillId: 'test', Chamber: 'Joint' })).toBe("Joint");
 
-        expect(inferChamber({ Id: 'test', OriginatingAgency: 'House' })).toBe("House");
-        expect(inferChamber({ Id: 'test', Name: 'SB101' })).toBe("Senate");
+        expect(inferChamber({ Id: 'test', BillId: 'test', OriginatingAgency: 'House' })).toBe("House");
+        expect(inferChamber({ Id: 'test', BillId: 'test', Name: 'SB101' })).toBe("Senate");
     });
 
     it("extractBillNumber", () => {
         expect(extractBillNumber()).toBe(undefined);
         expect(extractBillNumber('SB101')).toBe('101');
-        expect(extractBillNumber(null, { Id: 'test' })).toBe(undefined);
-        expect(extractBillNumber(null, { Id: 'test', Name: 'HH232' })).toBe('232');
-        expect(extractBillNumber('SB101', { Id: 'test', Description: 'HH232' })).toBe('101');
-        expect(extractBillNumber('bad', { Id: 'test', Name: 'sr242', Description: 'HH232' })).toBe('242');
-        expect(extractBillNumber('bad', { Id: 'test', Name: 'worse', Description: 'worst' })).toBe(undefined);
+        expect(extractBillNumber(null, { Id: 'test', BillId: 'test' })).toBe(undefined);
+        expect(extractBillNumber(null, { Id: 'test', BillId: 'test', Name: 'HH232' })).toBe('232');
+        expect(extractBillNumber('SB101', { Id: 'test', BillId: 'test', Description: 'HH232' })).toBe('101');
+        expect(extractBillNumber('bad', { Id: 'test', BillId: 'test', Name: 'sr242', Description: 'HH232' })).toBe('242');
+        expect(extractBillNumber('bad', { Id: 'test', BillId: 'test', Name: 'worse', Description: 'worst' })).toBe(undefined);
     });
 
     it("summarizeSponsors", () => {
-        expect(summarizeSponsors({ Id: 'test' })).toBe('');
-        expect(summarizeSponsors({ Id: 'test', Sponsors: [{ Name: 'bill' } as Sponsor, { Name: 'ted' } as Sponsor] })).toBe('bill, ted');
+        expect(summarizeSponsors({ Id: 'test', BillId: 'test' })).toBe('');
+        expect(summarizeSponsors({ Id: 'test', BillId: 'test', Sponsors: [{ Name: 'bill' } as Sponsor, { Name: 'ted' } as Sponsor] })).toBe('bill, ted');
     });
 
 
