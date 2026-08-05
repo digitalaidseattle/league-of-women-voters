de9cfecb5cfbece077e44229b1b9787286f2aa85
Tristan San Juan <tristansj88@gmail.com>
2026-08-05 13:48:58 -0700

    readjusted migrationfile

--- supabase/migrations/000002_adding_preferences.sql
+++ supabase/migrations/000002_adding_preferences.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS public."Preferences"(
   id uuid primary key default gen_random_uuid(),
-  biennium text not null default '2025-26',
-  bills jsonb not null default '[]' ::jsonb,
-  user_id uuid references auth.users(id) on delete cascade,
-  username text unique,
+  biennium text,
+  bills jsonb,
   created_at timestamp with time zone not null default now()
 ) TABLESPACE pg_default;
 
